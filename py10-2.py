69aef5e4be564338da3536719a7ecc5b80e8d5b3
Lily520 <Lily_super12580@163.com>
2017-09-17 14:22:11 +0800

    level2:实现方法2

---
+++ py10-2.py
@@ -0,0 +1,11 @@
+#实现方法2： 利用正则表达式
+#a = [1,11,21,1211,111221,...]
+
+import re
+
+if __name__ == "__main__":
+    a = "1"
+    for i in range(1,31):
+        next = [str(len(i + j)) + str(i) for i, j in re.findall(r"(\d)(\1*)", a)]  #（first appearance,following appearance）
+        a = "".join(next)
+    print(len(a)) #5808
